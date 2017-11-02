ea8c671e59a611a6fcaf74e8395a1864052e3d07
DiogoDantas <dioogoven@gmail.com>
2017-11-02 16:48:45 -0300

    Atualizando modelo de relacionamento do banco de dados

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Prova, Disciplina
-
-# Register your models here.
+from .models import Prova, Disciplina, Periodo, Curso
 
 admin.site.register(Prova)
 admin.site.register(Disciplina)
+admin.site.register(Periodo)
+admin.site.register(Curso)
--- api/models.py
+++ api/models.py
@@ -1,33 +1,32 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
-
-
-class Disciplina(models.Model):
-	id = models.AutoField(primary_key=True)
-	nome = models.CharField(max_length=150)
-
-	def __str__(self):
-		return self.nome
-
-class Curso(models.Model):
-	id = models.AutoField(primary_key=True)
-	disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
-	nome = models.CharField(max_length=150)
-
-	def __str__(self):
-		return self.nome
-
-
-class Prova(models.Model):
-    id = models.AutoField(primary_key=True)
-    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
-    periodo = models.CharField(max_length=100, blank=True, default='')
-    data_upload = models.DateTimeField(auto_now_add=True)
-    classificacao = models.IntegerField()
-    miniatura = models.TextField()
-    pdf_path = models.TextField()
+class Periodo(models.Model):
+    semestre = models.CharField(max_length=10)
 
     def __str__(self):
-    	return str(self.disciplina) + ' ' + self.periodo
+        return self.semestre
+
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nome
+
+class Disciplina(models.Model):
+    nome = models.CharField(max_length=150)
+    curso = models.ManyToManyField(Curso)
+    
+    def __str__(self):
+        return self.nome
+
+class Prova(models.Model):
+    pdf = models.FileField()
+    miniatura = models.ImageField(blank=True, null=True)
+    data_upload = models.DateTimeField(auto_now_add=True)
+    periodo = models.ForeignKey(Periodo, on_delete=models.CASCADE)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    classificacao = models.IntegerField()
+
+    def __str__(self):
+        return str(self.disciplina)
